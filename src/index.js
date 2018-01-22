4c64baaf03d6377af12923bb97a98ea3f24406be
requinard <d.diks94@gmail.com>
2018-01-22 19:31:06 +0100

    First dependencies

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux'
+import {applyMiddleware, createStore} from 'redux';
+import {logger} from 'redux-logger';
+import thunk from 'redux-thunk';
+import { actionStorageMiddleware, createStorageListener } from 'redux-state-sync';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const middlewares = [
+    actionStorageMiddleware,
+    thunk,
+    logger
+]
+
+let store = createStore(
+    () => {},
+    applyMiddleware(...middlewares)
+);
+
+createStorageListener(store);
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
